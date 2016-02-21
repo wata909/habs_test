c38a4feaafe7d90865413a528ffea46ea6f1025d
wata909 <wata909@gmail.com>
2016-02-21 18:01:23 +0900

    change!

--- 5239/html.bat
+++ 5239/html.bat
@@ -1,6 +1,6 @@
 #パワーシェルにコピペして実行出来る
 
-$html = Get-content .\rapid544000.html | % { $_ -replace "rapid523926.geojson", "rapid523927.geojson" } | % { $_ -replace "http://wata909.github.io/habs_test/geojson/", "http://wata909.github.io/habs_test/5239/geojson/" } 
+$html = Get-content .\rapid523926.html | % { $_ -replace "rapid523926.geojson", "rapid523927.geojson" } | % { $_ -replace "http://wata909.github.io/habs_test/geojson/", "http://wata909.github.io/habs_test/5239/geojson/" } 
 $html | Out-File rapid523927.html -Encoding UTF8
 $html = Get-content .\rapid544000.html | % { $_ -replace "rapid544000.geojson", "rapid543924.geojson" } | % { $_ -replace "http://wata909.github.io/habs_test/geojson/", "http://wata909.github.io/habs_test/5439/geojson/" } 
 $html | Out-File rapid543924.html -Encoding UTF8
