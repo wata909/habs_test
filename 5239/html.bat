fc216be5d3f8496e487a98227d6b97f877a8ad49
wata909 <wata909@gmail.com>
2016-02-21 17:58:49 +0900

    add 523927

--- 5239/html.bat
+++ 5239/html.bat
@@ -1,11 +1,7 @@
 #パワーシェルにコピペして実行出来る
 
-$html = Get-content .\rapid544000.html | % { $_ -replace "rapid544000.geojson", "rapid523926.geojson" } | % { $_ -replace "http://wata909.github.io/habs_test/geojson/", "http://wata909.github.io/habs_test/5239/geojson/" } 
-$html | Out-File rapid523926.html -Encoding UTF8
-
-
-$html = Get-content .\rapid544000.html | % { $_ -replace "rapid544000.geojson", "rapid543923.geojson" } | % { $_ -replace "http://wata909.github.io/habs_test/geojson/", "http://wata909.github.io/habs_test/5439/geojson/" } 
-$html | Out-File rapid543923.html -Encoding UTF8
+$html = Get-content .\rapid544000.html | % { $_ -replace "rapid523926.geojson", "rapid523927.geojson" } | % { $_ -replace "http://wata909.github.io/habs_test/geojson/", "http://wata909.github.io/habs_test/5239/geojson/" } 
+$html | Out-File rapid523927.html -Encoding UTF8
 $html = Get-content .\rapid544000.html | % { $_ -replace "rapid544000.geojson", "rapid543924.geojson" } | % { $_ -replace "http://wata909.github.io/habs_test/geojson/", "http://wata909.github.io/habs_test/5439/geojson/" } 
 $html | Out-File rapid543924.html -Encoding UTF8
 $html = Get-content .\rapid544000.html | % { $_ -replace "rapid544000.geojson", "rapid543912.geojson" } | % { $_ -replace "http://wata909.github.io/habs_test/geojson/", "http://wata909.github.io/habs_test/5439/geojson/" } 
