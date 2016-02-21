8c76c345684c55efb0ed89e4123f52c5d3a316b6
wata909 <wata909@gmail.com>
2016-02-21 18:25:50 +0900

    add geojson

--- 5239/html.bat
+++ 5239/html.bat
@@ -2,27 +2,39 @@
 
 $html = Get-content .\rapid523926.html | % { $_ -replace "rapid523926.geojson", "rapid523936.geojson" } | % { $_ -replace "http://wata909.github.io/habs_test/geojson/", "http://wata909.github.io/habs_test/5239/geojson/" } 
 $html | Out-File rapid523936.html -Encoding UTF8
+
 $html = Get-content .\rapid523926.html | % { $_ -replace "rapid523926.geojson", "rapid523937.geojson" } | % { $_ -replace "http://wata909.github.io/habs_test/geojson/", "http://wata909.github.io/habs_test/5239/geojson/" } 
 $html | Out-File rapid523937.html -Encoding UTF8
+
 $html = Get-content .\rapid523926.html | % { $_ -replace "rapid523926.geojson", "rapid523946.geojson" } | % { $_ -replace "http://wata909.github.io/habs_test/geojson/", "http://wata909.github.io/habs_test/5239/geojson/" } 
 $html | Out-File rapid523946.html -Encoding UTF8
+
 $html = Get-content .\rapid523926.html | % { $_ -replace "rapid523926.geojson", "rapid523947.geojson" } | % { $_ -replace "http://wata909.github.io/habs_test/geojson/", "http://wata909.github.io/habs_test/5239/geojson/" } 
 $html | Out-File rapid523947.html -Encoding UTF8
+
 $html = Get-content .\rapid523926.html | % { $_ -replace "rapid523926.geojson", "rapid523954.geojson" } | % { $_ -replace "http://wata909.github.io/habs_test/geojson/", "http://wata909.github.io/habs_test/5239/geojson/" } 
 $html | Out-File rapid523954.html -Encoding UTF8
+
 $html = Get-content .\rapid523926.html | % { $_ -replace "rapid523926.geojson", "rapid523955.geojson" } | % { $_ -replace "http://wata909.github.io/habs_test/geojson/", "http://wata909.github.io/habs_test/5239/geojson/" } 
 $html | Out-File rapid523955.html -Encoding UTF8
+
 $html = Get-content .\rapid523926.html | % { $_ -replace "rapid523926.geojson", "rapid523956.geojson" } | % { $_ -replace "http://wata909.github.io/habs_test/geojson/", "http://wata909.github.io/habs_test/5239/geojson/" } 
 $html | Out-File rapid523956.html -Encoding UTF8
+
 $html = Get-content .\rapid523926.html | % { $_ -replace "rapid523926.geojson", "rapid523957.geojson" } | % { $_ -replace "http://wata909.github.io/habs_test/geojson/", "http://wata909.github.io/habs_test/5239/geojson/" } 
 $html | Out-File rapid523957.html -Encoding UTF8
+
 $html = Get-content .\rapid523926.html | % { $_ -replace "rapid523926.geojson", "rapid523961.geojson" } | % { $_ -replace "http://wata909.github.io/habs_test/geojson/", "http://wata909.github.io/habs_test/5239/geojson/" } 
 $html | Out-File rapid523961.html -Encoding UTF8
+
 $html = Get-content .\rapid523926.html | % { $_ -replace "rapid523926.geojson", "rapid523964.geojson" } | % { $_ -replace "http://wata909.github.io/habs_test/geojson/", "http://wata909.github.io/habs_test/5239/geojson/" } 
 $html | Out-File rapid523964.html -Encoding UTF8
+
 $html = Get-content .\rapid523926.html | % { $_ -replace "rapid523926.geojson", "rapid523965.geojson" } | % { $_ -replace "http://wata909.github.io/habs_test/geojson/", "http://wata909.github.io/habs_test/5239/geojson/" } 
 $html | Out-File rapid523965.html -Encoding UTF8
+
 $html = Get-content .\rapid523926.html | % { $_ -replace "rapid523926.geojson", "rapid523966.geojson" } | % { $_ -replace "http://wata909.github.io/habs_test/geojson/", "http://wata909.github.io/habs_test/5239/geojson/" } 
 $html | Out-File rapid523966.html -Encoding UTF8
+
 $html = Get-content .\rapid523926.html | % { $_ -replace "rapid523926.geojson", "rapid523967.geojson" } | % { $_ -replace "http://wata909.github.io/habs_test/geojson/", "http://wata909.github.io/habs_test/5239/geojson/" } 
 $html | Out-File rapid523967.html -Encoding UTF8
